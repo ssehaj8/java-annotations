15b02ede7dfe65d6c323b5d8f515e02fa7423606
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-04-04 14:37:25 +0530

    [Sehajpreet] Add: a parent class Animal with a method makeSound(). Then, create a Dog class that overrides this method using @Override.

---
+++ src/main/java/com/bridgelabz/ExerciseProblems/AnimalSound.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.ExerciseProblems;
+
+public class AnimalSound {
+    static class Animal{
+        public void makeSound() {
+            System.out.println("Some geniric animal sound");
+        }
+    }
+
+    static class Dog extends Animal {
+        @Override
+        public void makeSound() {
+            System.out.println("Woof! Woof!");
+        }
+    }
+
+        public static void main(String[] args){
+            Animal myDog=new Dog();
+            myDog.makeSound();
+
+    }
+}
+/*
+Woof! Woof!
+
+ */
